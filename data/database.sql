69d3937764d8660f7315661ed86b1cc91e3bec87
MuriloGoms <murilogcordeiro08@gmail.com>
2026-03-28 02:38:30 -0300

    Recuperei o banco e arrumei alguns detalhes T-T

--- data/database.sql
+++ data/database.sql
@@ -1,15 +1,15 @@
-CREATE DATABASE COFFEE_SHOP;
-USE COFFEE_SHOP;
+CREATE DATABASE COFFEE_SHOPs;
+USE COFFEE_SHOPs;
 
 CREATE TABLE PRODUTOS (
-id INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(30) NOT NULL,
-quantidade INT default 0 NOT NULL,
-preco DOUBLE NOT NULL
+id_prod INT AUTO_INCREMENT PRIMARY KEY,
+nome_prod VARCHAR(30) NOT NULL,
+quantidade_prod INT default 0 NOT NULL,
+preco_prod DOUBLE NOT NULL
 );
 
 
-INSERT INTO PRODUTOS(nome, quantidade, preco) values
+INSERT INTO PRODUTOS(nome_prod, quantidade_prod, preco_prod) values
 ('Cappuccino_com_Canela', 50, 25.85),
 ('Cafe_Americano', 100, 20.99),
 ('Cha_Gelado_Limao', 44, 35.99),
@@ -22,15 +22,15 @@ INSERT INTO PRODUTOS(nome, quantidade, preco) values
 ('Cafe_Frappe', 43, 14.90),
 ('Pao_de_Queijo', 183, 4.90),
 ('Pao_na_Chapa', 144, 12.90);
-ALTER TABLE PRODUTOS MODIFY preco DECIMAL(10,2) NOT NULL;
+ALTER TABLE PRODUTOS MODIFY preco_prod DECIMAL(10,2) NOT NULL;
 
 SELECT * FROM PRODUTOS;
 
 CREATE TABLE USUARIO (
-id INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(100) NOT NULL,
-email VARCHAR(80) NOT NULL,
-senha VARCHAR(25) NOT NULL
+    id_user INT AUTO_INCREMENT PRIMARY KEY,
+    nome_user VARCHAR(100) NOT NULL,
+    email_user VARCHAR(80) NOT NULL,
+    senha_user VARCHAR(25) NOT NULL
 );
 
 SELECT * FROM USUARIO;
